e6e1f6c90c91f5aa7d6ac06a84e446e88d5fd659
Dhruv1249 <dhr1249.lm@gmail.com>
2026-04-09 12:13:41 +0530

    Changes md viewer

--- nvim/.config/nvim/lua/plugins/mdpreview.lua
+++ nvim/.config/nvim/lua/plugins/mdpreview.lua
@@ -1,9 +1,19 @@
-return{
-  "MeanderingProgrammer/render-markdown.nvim",
-  ft = { "markdown", "md" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons", -- optional but recommended
-  },
-  opts = {},
-}
+-- return{
+--   "MeanderingProgrammer/render-markdown.nvim",
+--   ft = { "markdown", "md" },
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-tree/nvim-web-devicons", -- optional but recommended
+--   },
+--   opts = {},
+-- }
+-- For `plugins/markview.lua` users.
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    preview = {
+        icon_provider = "devicons", -- "mini" or "devicons"
+    }
+ 
+};
