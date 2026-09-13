404d1d5fdcc947f7dd93da532d101f8e6c31b89a
Dhruv1249 <dhr1249.lm@gmail.com>
2026-09-13 20:58:55 +0530

    fix: made transparency work

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,17 +2,29 @@ require("config.options")
 
 local highlights = require("config.highlights")
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "dms",
-    callback = function()
-        vim.schedule(highlights.apply)
-    end,
-})
+local function transparent()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+
 require("config.lazy")
 require("config.keybinds")
 require("config.diagnostics")
 
-
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "dms",
+    callback = function()
+        vim.schedule(function()
+            highlights.apply()
+            transparent()
+        end)
+    end,
+})
 
 vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
@@ -20,12 +32,7 @@ vim.api.nvim_create_autocmd("User", {
         vim.schedule(function()
             vim.cmd.colorscheme("dms")
             highlights.apply()
+            transparent()
         end)
     end,
 })
-
-
-highlights.apply()
-
--- vim.o.winblend = 10
--- vim.o.pumblend = 10
